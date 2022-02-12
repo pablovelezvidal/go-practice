43993c08df37d16ff87ef78cdf90122b266c2a2d
Pablo Velez Vidal <pablo.velez@mattermost.com>
2022-02-12 14:55:02 +0100

    add the wait group to know when the routines have finished

--- go_simplified/go_routines/main.go
+++ go_simplified/go_routines/main.go
@@ -3,19 +3,23 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
 
 func main() {
+	wg := &sync.WaitGroup{}
 	for i := 0; i < 10; i++ {
-		go showGoRoutine(i)
+		wg.Add(1)
+		go showGoRoutine(i, wg)
 	}
-	time.Sleep(3 * time.Second)
+	wg.Wait()
 }
 
-func showGoRoutine(id int) {
+func showGoRoutine(id int, wg *sync.WaitGroup) {
 	delay := rand.Intn(500)
 	fmt.Printf("Go routine #%d\n", id)
 
 	time.Sleep(time.Millisecond * time.Duration(delay))
+	wg.Done()
 }
