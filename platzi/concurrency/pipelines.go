92cf29267abf240922d90a8d731e53da536b9f03
Pablo Velez Vidal <pablo.velez@mattermost.com>
2022-03-03 11:25:00 -0500

    add comments to better understand what the code is doing

--- platzi/concurrency/pipelines.go
+++ platzi/concurrency/pipelines.go
@@ -8,13 +8,16 @@ func main() {
 	generator := make(chan int)
 	doubles := make(chan int)
 
+	// new process adding values to the generator channel
 	go Generator(generator)
+	// new process reading the generator values, doubling them and writing to the doubles channel
 	go Double(generator, doubles)
 
 	Print(doubles)
 }
 
 func Generator(c chan<- int) {
+	// fill the generator channel with numbers
 	for i := 1; i <= 10; i++ {
 		c <- i
 	}
@@ -22,6 +25,7 @@ func Generator(c chan<- int) {
 }
 
 func Double(in <-chan int, out chan<- int) { // canal escritura, flechita a la derecha de chan, lectura va a la izquierda
+	// read in (generator channel) loop through it, double it and write it to the out channel (doubles)
 	for value := range in {
 		out <- 2 * value
 	}
@@ -29,6 +33,7 @@ func Double(in <-chan int, out chan<- int) { // canal escritura, flechita a la d
 }
 
 func Print(out chan int) {
+	// read all values in the output channel (doubles) and print it
 	for value := range out {
 		fmt.Println(value)
 	}
--- platzi/concurrency/worker_pools.go
+++ platzi/concurrency/worker_pools.go
@@ -5,30 +5,40 @@ import (
 )
 
 func main() {
+	// controled defined number of tasks in this case is just integer numbers
 	tasks := []int{1, 2, 3, 4, 5, 7, 10, 12, 40}
+	// number of workers (processes that will be doing the tasks)
 	nWorkers := 3
+	// channel that will contain the information
 	jobs := make(chan int, len(tasks))
+	// channel that will store the output result of the process
 	results := make(chan int, len(tasks))
 
 	for i := 0; i < nWorkers; i++ {
+		// create the process that will run the tasks
 		go Worker(i, jobs, results)
 	}
 
+	// fill the jobs process with the values
 	for _, value := range tasks {
 		jobs <- value
 	}
 	close(jobs)
 
+	// free the values from the results channel
 	for r := 0; r < len(tasks); r++ {
 		<-results
 	}
 }
 
 func Worker(id int, jobs <-chan int, results chan<- int) {
+	// iterate over the jobs channel
 	for job := range jobs {
 		fmt.Printf("Workers started with id %d \n", id)
+		// do the work based on the values in the jobs channel
 		fib := Fibo(job)
 		fmt.Printf("Worker finished, job: %d and fibo result %d \n", job, fib)
+		// send the reulting value to the results channel
 		results <- fib
 	}
 }
